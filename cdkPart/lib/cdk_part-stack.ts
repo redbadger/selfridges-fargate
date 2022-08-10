1dcee992f1929f33712b01465158bc6e08d2665d
Tim Lee <tim.lee@red-badger.com>
2022-08-10 09:26:43 +0100

    Add cdk setup

---
+++ cdkPart/lib/cdk_part-stack.ts
@@ -0,0 +1,36 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as path from "path";
+
+export class CdkPartStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "SelfridgesFargateVpc", {
+      maxAzs: 3, // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "SelfridgesFargateCluster", {
+      vpc: vpc,
+    });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "SelfridgesFargateDemo",
+      {
+        cluster: cluster,
+        desiredCount: 1, // Default is 1,
+        cpu: 512,
+        memoryLimitMiB: 1024,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+        },
+        publicLoadBalancer: true, // Default is false,
+        assignPublicIp: true,
+      }
+    );
+  }
+}
---
+++ cdkPart/test/cdk_part.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPart from '../lib/cdk_part-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_part-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPart.CdkPartStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
